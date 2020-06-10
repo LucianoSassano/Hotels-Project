b56ea51a932b45d74b77c5033aee75bed9935299
luciano sassano <54543892+LucianoSassano@users.noreply.github.com>
2020-06-10 20:29:34 -0300

    Update CountryController.java

--- src/main/java/com/example/demo/controller/CountryController.java
+++ src/main/java/com/example/demo/controller/CountryController.java
@@ -10,34 +10,35 @@ import org.springframework.web.bind.annotation.*;
 import javax.persistence.Column;
 import java.util.List;
 
+@RequestMapping(path = "/countries")
 @RestController
 public class CountryController {
 
     @Autowired
     private CountryService countryService;
 
-    @GetMapping("/countries")
+    @GetMapping
     public ResponseEntity<List<Country>> getAllCountries() {
         return ResponseEntity.ok().body(countryService.listAllCountries());
     }
 
-    @GetMapping("/countries/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Country> findCountryById(@PathVariable Integer id){
         return ResponseEntity.ok().body(countryService.getById(id));
     }
 
-    @PostMapping("/countries")
+    @PostMapping
     public ResponseEntity<Country> createCountry(@RequestBody Country country){
         return ResponseEntity.ok().body(countryService.create(country));
     }
 
-    @PutMapping("/countries/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Country> updateCountry(@PathVariable Integer id , @RequestBody Country country){
         country.setId(id);
         return ResponseEntity.ok().body(this.countryService.updateCountry(country));
     }
 
-    @DeleteMapping("countries/{id}")
+    @DeleteMapping("/{id}")
     public HttpStatus deleteCountry(@PathVariable Integer id){
         this.countryService.deleteCountryById(id);
         return HttpStatus.OK;
