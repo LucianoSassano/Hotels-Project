3daa413640da3fa36ff049b879459d7e64a19240
luciano sassano <54543892+LucianoSassano@users.noreply.github.com>
2020-06-10 20:34:45 -0300

    Update City.java

--- src/main/java/com/example/demo/model/City.java
+++ src/main/java/com/example/demo/model/City.java
@@ -12,11 +12,11 @@ public class City {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+   
     private Integer id;
-    @Column(name = "name")
+   
     private String name;
-    @Column(name = "zipCode")
+    
     private Integer zipCode;
 
     @ManyToOne
