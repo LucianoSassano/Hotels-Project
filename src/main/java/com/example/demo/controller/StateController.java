e74c9c46e8f752431a61f97a4964d4d1edf1ddb3
luciano sassano <54543892+LucianoSassano@users.noreply.github.com>
2020-06-10 20:34:03 -0300

    Update StateController.java
    Delete method added in the following pr

--- src/main/java/com/example/demo/controller/StateController.java
+++ src/main/java/com/example/demo/controller/StateController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequestMapping(path = "/states")
 @RestController
 public class StateController {
 
@@ -16,22 +17,22 @@ public class StateController {
     private StateService stateService;
 
 
-    @GetMapping("/states")
+    @GetMapping
     public ResponseEntity<List<Estate>> getAllStates(){
         return ResponseEntity.ok().body(this.stateService.listAllStates());
     }
 
-    @GetMapping("/states/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Estate> getStateById(@PathVariable Integer id){
         return ResponseEntity.ok().body(this.stateService.getById(id));
     }
 
-    @PostMapping("/states")
+    @PostMapping
     public ResponseEntity<Estate> createState(@RequestBody Estate state){
         return ResponseEntity.ok().body(this.stateService.create(state));
     }
 
-    @PutMapping("/states/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Estate> updateState(@PathVariable Integer id,@RequestBody Estate state){
         state.setId(id);
         return ResponseEntity.ok().body(this.stateService.updateState(state));
