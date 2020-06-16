2c3641b12cb7ef07c57af3f551b447bf2083146f
luciano sassano <54543892+LucianoSassano@users.noreply.github.com>
2020-06-16 18:08:46 -0300

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
-import com.example.demo.model.City;
-import com.example.demo.model.Country;
-import com.example.demo.model.Estate;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
